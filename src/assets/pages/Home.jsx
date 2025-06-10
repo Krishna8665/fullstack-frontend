e7208f61ecfde5b798cc95b51d07802a690f01cf
Krishna8665 <khatrikrissna11@gmail.com>
2025-06-10 13:37:37 +0545

    initial commit

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../components/Button'
 import Navbar from '../components/Navbar'
 import Card from '../components/Card'
+import axios from 'axios'
 
 function Home() {
-  console.log(
-    "hello world"
-  )
-  return (
-    <>
-    <Navbar/>
-    <div className="flex flex-wrap ">
+  const [books, setBooks] = useState([])
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3000/api/books/")
+    setBooks(response.data.datas) // response.data   chai lekhnai parcha hai compulsary tesapchi k lyaune ho . pachi lekhne console ki bowser n/w ma herne
+  }
+    
+    
+    useEffect(() => {
+      fetchBooks()
 
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    <Card/>
-    </div>
+    }, [])
 
-    </>
-  )
-}
+    return (
+      <>
+        <Navbar />
+        <div className="flex flex-wrap ">
+          {
+            books.map(function (book) {
+              return (
 
-export default Home
+                <Card book={book}/>
+              )
+
+            })
+
+          }
+
+        </div>
+
+      </>
+    )
+
+  }
+
+
+  export default Home
